61f09f2581970714e2d8616bdbd8e6460d364c14
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-10-24 18:30:18 +0200

    Use init.sh to start manager_gui, it install all the dependecy and launches the manager server

--- manager_gui/init.sh
+++ manager_gui/init.sh
@@ -3,19 +3,17 @@
 
 
 
-./installNodejs.sh
 ./installZeroRPC.sh
-# ./installGraphite.sh
-# ./installMongoBD.sh
+./installNodejs.sh
+./installGraphite.sh up &
+./installMongoBD.sh &
 
 
 # init manager-rpc server
 echo "start manager RPC Server"
 python manager.py &
 
-
-
 # init node server
 echo "start manager GUI Server"
-
 npm start
+
