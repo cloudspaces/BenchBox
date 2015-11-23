8d2dacab0321aaa3dc4fd17e6e992760df73d7d8
chenglong zou <chenglong.zq@gmail.com>
2015-11-23 23:12:27 +0100

    Add nohup to python otherwse the following line will never be reached

--- vagrant/startPeerConsumer.sh
+++ vagrant/startPeerConsumer.sh
@@ -6,5 +6,5 @@ if [ -f /tmp/prod_status.pid ]
     kill -9 $line
 fi
 echo "#4 Start prod_status"
-python prod_status.py --msg setupFinished --topic `hostname`
+nohup python prod_status.py --msg setupFinished --topic `hostname`
 echo $! > /tmp/prod_status.pid
