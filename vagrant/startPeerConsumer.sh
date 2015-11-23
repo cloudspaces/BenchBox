42ca4c0e51cdb280f97c5f30944ba9ab34a45ad0
chenglong zou <chenglong.zq@gmail.com>
2015-11-23 20:38:01 +0100

    Removed kill 0 try at startPeer Consumer

--- vagrant/startPeerConsumer.sh
+++ vagrant/startPeerConsumer.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ -f prod_status.pid ]
     then
     line=($(<"prod_status.pid"))
-    kill 0 $line
-    if [ $? -eq 0 ]
-        then
-        kill -9 $line
-    fi
+    kill -9 $line
 fi
 echo "#4 Start prod_status"
 python prod_status.py --msg setupFinished --topic `hostname`
 echo $! > prod_status.pid
-
