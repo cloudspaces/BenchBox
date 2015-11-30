420a0b74d8f05a8b1a47cdef6ebc29600b987083
chenglong zou <chenglong.zq@gmail.com>
2015-11-30 18:44:47 +0100

    Switch Graphite to Grafana, change carbon into influxdb

---
+++ vagrant/emit_metric.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import pika
+import uuid
+import urlparse
+import sys
+import time
+
+class EmitMetric(object):
+    def __init__(self):
+        url_str = None
+        with open('rabbitmq','r') as r:
+            url_str = r.read().splitlines()[0]
+
+        url = urlparse.urlparse(url_str)
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=url.hostname,
+            virtual_host=url.path[1:],
+            credentials=pika.PlainCredentials(url.username, url.password)
+        ))
+
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='metrics', type='fanout')
+
+
+    def emit(self, key, value):
+        msg = '{} {} {}'.format(key, value, self.tsNow())
+        print msg
+        self.channel.basic_publish(
+            exchange='metrics',
+            routing_key='Joker',
+            body=msg)
+
+    def tsNow(self):
+        return int(time.time())
+
+
+if __name__ == '__main__':
+    print " [x] emit(key, value)"
+    print " [x] emit({}, {})".format(sys.argv[1], sys.argv[2])
+    metric_emiter = EmitMetric()
+    response = metric_emiter.emit(sys.argv[1], sys.argv[2])
