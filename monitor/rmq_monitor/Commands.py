219aefbb6e9984502dab8943789a804805f063f1
chenglong <chenglong.zq@gmail.com>
2016-03-16 16:05:11 +0100

    Add monitor rmq files

---
+++ monitor/rmq_monitor/Commands.py
@@ -0,0 +1,168 @@
+import os
+import time
+import subprocess
+import signal
+from threading import Thread
+import datetime
+import psutil
+from termcolor import colored
+from rmq_monitor.EmitMetric import EmitMetric
+
+
+class Commands(object):
+    # singleton class
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Commands, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, hostname):
+        """
+        This script contains the thread operations of a monitor process contained in the sandBox.
+        :param hostname: Name of the current dummy host
+        :return:
+        """
+        print '[INIT_MONITOR_RMQ]: rpc commands'
+        self.hostname = hostname
+
+        self.is_running = False     # state flag
+
+        self.stereotype = None      # backupsample
+        self.monitor = None         # monitor_process => experimento
+        self.sync_client = None     # sync_client process
+        self.sync_proc_pid = None   # GATHER THE SYNC PID FROM SYNC PROC, actively updated by _pc_client
+        self.sync_proc = None
+        self.personal_cloud = None  # personal cloud
+        self.executor_state = "Unknown"  # state +  time
+
+    def hello(self, body):
+        print '[HELLO]: hello world {}'.format(body['cmd'])
+        return '[HELLO]: hello world response'
+
+    def warmup(self, body):
+        """
+        This stage can be avoided, todo refactor this into start&stop
+        where start will automatically manage
+        - the syncronization client state [it periodically checks if the client is running]
+        - the syncronization client to use [it actively swaps between sincronization client]
+        :param body: contains the configuration and settings emited by the manager node
+        :return:
+        """
+        print '[WARMUP]: init personal cloud client'
+        return '[WARMUP]: warm up response'
+
+    def _test(self):
+        '''
+        Aixo ha d'executarse en un thread com a bucle infinit
+        :return:
+        '''
+        operations = 0
+        metric_reader = EmitMetric(
+                hostname=self.hostname,
+                personal_cloud=self.personal_cloud,
+                receipt=self.stereotype)
+
+        # define metric reader with [hostname & sync client name]
+        while self.is_running:
+            operations += 1
+            metric_reader.emit(self.sync_proc_pid)  # send metric to rabbit
+            time.sleep(2)  # delay between metric
+            print colored("[TEST]: INFO {} --> {} // {} // {}"
+                          "".format(time.ctime(time.time()), operations, self.is_running), 'red')
+        print "QUIT emit metrics!!!"
+
+
+    def _pc_client(self):
+        """
+        This thread will get track the personal client process id, and actively
+        keep the pc client running {self.sync_proc_pid}
+        :return:
+        """
+        pc_cmd = {
+            'stacksync': "/usr/bin/java -jar /usr/lib/stacksync/Stacksync.jar -d -c /vagrant",
+            'owncloud': "",
+            'dropbox': "/home/vagrant/.dropbox-dist/dropboxd"  # launch dropbox
+        }
+        str_cmd = pc_cmd[self.personal_cloud.lower()]
+
+        while self.is_running:
+            pid = None
+            while pid is None:
+                time.sleep(3)
+                if self.personal_cloud.lower() == 'dropbox':
+                    pid = int(subprocess.check_output(['pidof','dropbox']).replace('\n',''))
+                elif self.personal_cloud.lower() == 'stacksync':
+                    pid = int(subprocess.check_output(['pgrep','java']).replace('\n',''))
+                elif self.personal_cloud.lower() == 'owncloud':
+                    pid = int(subprocess.check_output(['pgrep','owncloud']).replace('\n',''))
+                else:
+                    print "misc client"
+
+                if pid is None:
+                    self.sync_proc = subprocess.Popen(str_cmd, shell=True)
+                else:
+                    try:
+
+                        p = psutil.Process(pid)
+                    except:
+                        print "{} None valid pid".format(pid)
+                        pid = None
+
+            self.sync_proc_pid = pid
+            time.sleep(5)
+
+            # every 5 seconds checks if the target personal cloud client is running
+
+    def start(self, body):
+        """
+        Start sending metrics
+        :param body:
+        :return:
+        """
+        print '[START_TEST] {}'.format(body)
+
+        if self.is_running:
+            return '[START_TEST]: INFO: already running!'
+        else:
+            self.is_running = True
+            # SELF THREAD START
+            self.personal_cloud = body['msg']['test']['testClient']
+            self.stereotype     = body['msg']['test']['testProfile']  # if its defined then this will be loaded
+            print '[START_TEST]: INFO: instance thread'
+            self.sync_client = Thread(target=self._pc_client)
+            self.sync_client.start()
+            # self.sync_client.start()
+            self.monitor = Thread(target=self._test)
+            self.monitor.start()
+            return '[START_TEST]: SUCCESS: run test response'
+
+    def stop(self, body):
+        if self.is_running:
+            print '[STOP_TEST]: stop test {}'.format(body)
+            self.is_running = False
+            self.monitor.join()
+            self.sync_client.join()
+            parent = psutil.Process(self.sync_proc_pid)
+            for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+                print child.kill()
+            print parent.kill()
+            self.executor_state = "monitor stop Capturing... "
+            return '[STOP_TEST]: SUCCESS: stop test'
+        else:
+            return '[STOP_TEST]: WARNING: no test is running'
+            pc_cmd = {
+                'stacksync': "java",
+                'owncloud': "",
+                'dropbox': "dropbox"  # launch dropbox
+            }
+            str_cmd = pc_cmd[self.personal_cloud.lower()]
+            pstring = str_cmd
+            for line in os.popen("ps ax | grep " + pstring + " | grep -v grep"):
+                fields = line.split()
+                proc_pid = fields[0]
+                os.kill(int(proc_pid), signal.SIGKILL)
+
+    def keepalive(self, body):
+        return "{} -> {}".format(datetime.datetime.now().isoformat(), self.executor_state)
---
+++ monitor/rmq_monitor/EmitMetric.py
@@ -0,0 +1,148 @@
+import calendar
+import json
+import random
+import urlparse
+
+import math
+import psutil
+import pika
+import time
+
+import subprocess
+
+
+class EmitMetric(object):
+
+    def __init__(self, hostname="", personal_cloud="", receipt=""):
+
+        self.personal_cloud = personal_cloud
+        self.receipt=receipt
+        pc_folders = {
+            'stacksync': 'stacksync_folder',
+            'dropbox': 'Dropbox'
+        }
+
+        self.personal_folder = pc_folders[self.personal_cloud.lower()]
+
+        self.hostname = hostname
+        self.proc = None
+        self.prev_metric = None # keep track of last emited metric
+        url_str = None
+        with open('rabbitmq','r') as r:
+            url_str = r.read().splitlines()[0]
+
+        self.prev_net_counter = psutil.net_io_counters(pernic=True)['eth0']  # 10.0.2.15 static for each sandBox
+
+        url = urlparse.urlparse(url_str)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host=url.hostname,
+                heartbeat_interval=5,
+                virtual_host=url.path[1:],
+                credentials=pika.PlainCredentials(url.username, url.password)
+        ))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='metrics', type='fanout')
+
+    def emit(self, pid):
+        """
+        Lookup for the process pid and emit metric
+        if exists emit metric
+        otherwise skip emit noop
+        :param proc: the syncronization client process.
+        :return:
+        """
+
+        metrics = {'cpu': 0,
+                   'ram': 1,
+                   'net': 2,
+                   'bytes_sent': 1,
+                   'bytes_recv': 1,
+                   'packets_sent': 1,
+                   'packets_recv': 1,
+                   'errin': 0,
+                   'errout': 0,
+                   'dropin': 0,
+                   'dropout': 0,
+                   'disk': 0,
+                   'files': 0,
+                   'time': calendar.timegm(time.gmtime()) * 1000}
+        # psutil read metrics
+        try:
+            self.proc = psutil.Process(pid)
+        except Exception as ex:
+            print ex.message
+            return 0 # exit as the process is not alive.
+
+        print "PID: {} [{}]".format(pid, self.personal_cloud.lower())
+        try:
+            if self.personal_cloud.lower() == "stacksync":
+                # todo lookup for stacksync process here => using psutil
+                cpu_usage = int(math.ceil(self.proc.cpu_percent(None)))
+                ram_usage = self.proc.memory_info().rss
+                metrics['cpu'] = cpu_usage
+                metrics['ram'] = ram_usage
+            elif self.personal_cloud.lower() == "owncloud":
+                print "TODO owncloud"
+            elif self.personal_cloud.lower() == "dropbox":
+                # todo lookup for dropbox process here => using psutil
+                cpu_usage = int(math.ceil(self.proc.cpu_percent(None)))
+                ram_usage = self.proc.memory_info().rss
+                metrics['cpu'] = cpu_usage
+                metrics['ram'] = ram_usage
+        except Exception as e:
+            print e.message
+
+        # assign the network usage metric
+
+        if self.prev_metric is not None:
+            # do nothing because its the first emit ant there are no previous metric to compare
+            # last_net = self.prev_metric['metrics']['net']
+            last_time = self.prev_metric['metrics']['time']
+
+            curr_net_counter = psutil.net_io_counters(pernic=True)['eth0']  # read the bytes from somewhere
+            curr_time = metrics['time']
+            elapsed_time = (curr_time-last_time) / 1000  # segons
+            for key, value in curr_net_counter.__dict__.items():
+                metrics[key] = (value - getattr(self.prev_net_counter, key)) / elapsed_time  # unit is seconds
+            self.prev_net_counter = curr_net_counter
+        # assign hard drive usage metric
+        drive_usage_cmd =['/usr/bin/du', '-ks', '/home/vagrant/{}'.format(self.personal_folder)]
+        proc = subprocess.Popen(drive_usage_cmd, stdout=subprocess.PIPE)
+        tmp = self.proc.stdout.read()
+        try:
+            metrics['disk'] = int(tmp.split('\t')[0])  # kilo bytes cast string to int
+        except:
+            print "invalid literal for... memory unit"
+            metrics['disk'] = 1
+        # assign add folder num of files metric
+        find_cmd = '/usr/bin/find /home/vagrant/{} -type f'.format(self.personal_folder).split()
+        proc_find = subprocess.Popen(find_cmd, stdout=subprocess.PIPE)
+        wc_cmd = '/usr/bin/wc -l'.split()
+        proc_wc = subprocess.Popen(wc_cmd, stdin=proc_find.stdout, stdout=subprocess.PIPE)
+        num_files = proc_wc.communicate()[0]
+        metrics['files'] = int(num_files.split('\t')[0])
+
+        tags = ''
+        if tags == '':
+            tags = {
+                'profile': self.receipt,
+                'credentials': 'pc_credentials',
+                'client': self.personal_cloud.lower(),
+            }
+
+        data = {
+            'metrics': metrics,
+            'tags': tags
+        }
+        self.prev_metric = data  # update the last emited metric
+
+        msg = json.dumps(data)
+        print msg
+
+        self.channel.basic_publish(
+                exchange='metrics',
+                routing_key=self.hostname,
+                body=msg)
+
+
+
