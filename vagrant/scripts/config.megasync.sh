d94135e8214f76f1d9f14e8cfa5f3a15e26b95de
chenglong <chenglong.zq@gmail.com>
2016-03-23 14:19:11 +0100

    Added megasync configuration file

---
+++ vagrant/scripts/config.megasync.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+
+echo `pwd`
+
+megaPidFile='/tmp/MegaSync.pid'
+
+
+FILE='megasync.sh'
+
+cat > $FILE <<- EOM
+#!/usr/bin/env bash
+
+
+if [ ! -f $megaPidFile]
+then
+        echo 'Run the client [MEGA]'
+else
+        echo 'Restart the client [MEGA]'
+        pid=\$(head -n 1 $megaPidFile)
+        echo 'status'
+        ps -p \$pid
+        status=\$?
+
+        if [ \$status -ne 0 ]
+        then
+                echo 'no such proc [MEGA]'
+        else
+                echo 'proc exists [MEGA]'
+                kill -9 \$pid
+        fi
+fi
+        echo \$PPID > $megaPidFile
+
+if [ \$# -eq 1 ]
+then
+        delay=\$1
+else
+        delay=1
+fi
+
+echo 'Create the mega folder'
+megacmd mkdir mega:/$1
+
+while true; do
+        echo 'DoSync'
+        megacmd sync /home/vagrant/mega_folder mega:/$1
+        echo 'SyncingComplete'
+        sleep \$delay
+done
+
+EOM
+
+
+if [[ -s $FILE ]] ; then
+echo "$FILE CREATED!!!"
+else
+echo "$FILE FAIL!?."
+fi ;
+
+
+
+ls -l $FILE
+echo 'New credentials generated successfully!!'
+chmod u+x $FILE
+ls $FILE
