0c23a6baa6b209742e21d3bc2af11d826f6b1ce6
chenglong <chenglong.zq@gmail.com>
2016-04-25 18:31:41 +0200

    Refactor manager stop if clause

--- manager/manager.stop.sh
+++ manager/manager.stop.sh
@@ -11,23 +11,23 @@ sudo service rabbitmq-server stop
 echo 'Stop the zeroRPC server'
 # python zerorpc/startZeroRPC.py &
 line=$(ps aux | grep -v grep |  grep zerorpc | awk '{print $2}')
-if [ $? -eq 0 ]
+if [ -z "$line" ]
     then
+    echo "No results"
+    else
     echo "Have result"
     kill -9 $line
-    else
-    echo "No results"
 fi
 
 
 echo 'Start the nodeManager server'
 line=$(ps aux | grep -v grep |  grep ./bin/www | awk '{print $2}')
-if [ $? -eq 0 ]
+if [ -z "$line" ]
     then
+    echo "No results"
+    else
     echo "Have result"
     kill -9 $line
-    else
-    echo "No results"
 fi
 
 echo 'Start/OK'
