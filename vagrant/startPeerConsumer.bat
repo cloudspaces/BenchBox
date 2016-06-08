665971e5bd47e6569fb0f38dcf2633da5609c4ee
chenglong zq <chenglong.zq@gmail.com>
2016-06-08 11:32:18 +0200

    Add startPeerConsumer.bat for windows sandBox machine

--- vagrant/startPeerConsumer.bat
+++ vagrant/startPeerConsumer.bat
@@ -2,7 +2,7 @@
 
 
 echo "Starting sandBox prod status"
-start nohup python C:\Users\vagrant\monitor\prod_status.py --msg provisionOK --topic %ComputerName%
+start nohup python C:\Users\vagrant\vagrant\startPeerConsumer.py --msg provisionOK --topic %ComputerName%
 
 
 
