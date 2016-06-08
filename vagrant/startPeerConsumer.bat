fd26f25eb394424a007762e9f30c0b95a893e04e
chenglong zq <chenglong.zq@gmail.com>
2016-06-08 14:39:24 +0200

    Disable puppet provision instead build single bat file

--- vagrant/startPeerConsumer.bat
+++ vagrant/startPeerConsumer.bat
@@ -2,10 +2,17 @@
 
 
 echo "Starting sandBox prod status"
-start /min python C:\Users\vagrant\vagrant\startPeerConsumer.py --msg provisionOK --topic %ComputerName%
+
+
+echo "Update"
+start /min python C:\Users\vagrant\vagrant\prod_status.py --msg provisionOK --topic %ComputerName%
+echo "Finish provisioning windows sandBox"
 
 
 
+:: this is a batch comment
+
+echo "How to know if the startPeerConsumer is running??"
 
 
 
