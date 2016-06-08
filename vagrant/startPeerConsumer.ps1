a68b92726d39146aec9f92bd139f4c1c323e957d
chenglong zq <chenglong.zq@gmail.com>
2016-06-08 17:42:24 +0200

    Create manager looper

---
+++ vagrant/startPeerConsumer.ps1
@@ -0,0 +1,10 @@
+Function Start-Monitoring
+{
+    While ($true)
+    {
+        # Do things lots
+
+        # Add a pause so the loop doesn't run super fast and use lots of CPU        
+        Start-Sleep 30
+    }
+}
